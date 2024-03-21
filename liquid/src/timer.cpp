3790cd45edb32e50cece0afcc59ee8f98bee3531
TempusGPT <contact@changyong.me>
2024-03-21 18:44:36 +0900

    Refactor timer

--- liquid/src/timer.cpp
+++ liquid/src/timer.cpp
@@ -1,17 +1,13 @@
 #include "include/timer.hpp"
 
+#include <set>
+
 namespace Liquid {
+    struct Timer;
+    static auto timerSet = std::multiset<Timer>();
+    static auto timerId = 0;
+
     struct Timer {
-        struct Comparator {
-            bool operator()(const Timer &lhs, const Timer &rhs) const {
-                return lhs.invokeAt > rhs.invokeAt;
-            }
-        };
-
-        static std::unordered_set<int> idSet;
-        static std::priority_queue<Timer, std::vector<Timer>, Timer::Comparator> queue;
-        static int newId;
-
         int id;
         bool willRepeat;
         std::function<void()> callback;
@@ -19,53 +15,54 @@ namespace Liquid {
         std::chrono::steady_clock::time_point invokeAt;
 
         Timer(bool willRepeat, int delay, const std::function<void()> &callback)
-            : id(newId++), willRepeat(willRepeat), callback(callback) {
+            : id(timerId++), willRepeat(willRepeat), callback(callback) {
             const auto now = std::chrono::steady_clock::now();
             const auto chronoDelay = std::chrono::milliseconds(delay);
             this->delay = chronoDelay;
             this->invokeAt = now + chronoDelay;
         }
-    };
 
-    std::unordered_set<int> Timer::idSet = {};
-    std::priority_queue<Timer, std::vector<Timer>, Timer::Comparator> Timer::queue = {};
-    int Timer::newId = 1;
+        bool operator<(const Timer &other) const {
+            return invokeAt < other.invokeAt;
+        }
+    };
 
     int setTimeout(int delay, const std::function<void()> &callback) {
         const auto timer = Timer(false, delay, callback);
-        Timer::queue.push(timer);
-        Timer::idSet.insert(timer.id);
+        timerSet.insert(timer);
         return timer.id;
     }
 
     int setInterval(int delay, const std::function<void()> &callback) {
         const auto timer = Timer(true, delay, callback);
-        Timer::queue.push(timer);
-        Timer::idSet.insert(timer.id);
+        timerSet.insert(timer);
         return timer.id;
     }
 
     void clearTimer(int id) {
-        Timer::idSet.erase(id);
+        for (auto it = timerSet.begin(); it != timerSet.end();) {
+            if (it->id == id) {
+                it = timerSet.erase(it);
+            } else {
+                ++it;
+            }
+        }
     }
 
     namespace Internal {
         void processTimer() {
             const auto now = std::chrono::steady_clock::now();
-            if (Timer::queue.empty() || Timer::queue.top().invokeAt > now) {
+            if (timerSet.empty() || timerSet.begin()->invokeAt > now) {
                 return;
             }
 
-            auto timer = Timer::queue.top();
-            Timer::queue.pop();
-            if (Timer::idSet.count(timer.id) == 0) {
-                return;
-            }
-
+            auto timer = *timerSet.begin();
+            timerSet.erase(timer);
             timer.callback();
+
             if (timer.willRepeat) {
                 timer.invokeAt += timer.delay;
-                Timer::queue.push(timer);
+                timerSet.insert(timer);
             }
         }
     }
--- src/components/Timer.hpp
+++ src/components/Timer.hpp
@@ -6,10 +6,15 @@
 using namespace Liquid;
 
 Element Timer(const Prop<std::string> &title) {
+    auto effect = createEffect();
     auto elapsed = createSignal(0);
 
-    setInterval(1000, [=]() mutable {
+    const auto id = setInterval(1000, [=]() mutable {
         elapsed.set(elapsed() + 1);
+    });
+
+    effect.cleanup([=]() {
+        clearTimer(id);
     });
 
     return Text(FN("%0%s in %1%\n"_f % elapsed() % title()), Color::Black, Color::White);
